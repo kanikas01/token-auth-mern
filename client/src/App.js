ff7efdc7ed391f2a04706822397fae5f8fd90b7e
kanikas01 <larvakurt@yahoo.com>
2019-05-25 14:35:35 -0700

    Add User model functionality, save hashed passwords

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Secret from './components/Secret';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/secret">Secret</Link></li>
+          </ul>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/secret" component={Secret} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
--- server.js
+++ server.js
@@ -2,14 +2,51 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const mongoose = require('mongoose');
+const mongo_uri = 'mongodb://localhost/react-auth';
+const User = require('./models/User.js');
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+// Middleware
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
+  extended: true
+}));
+
+// Routes
+app.get('/api/home', function(req, res) {
+  res.send('Welcome!');
+});
+
+app.get('/api/secret', function(req, res) {
+  res.send('The password is potato');
 });
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// POST route to register a user
+app.post('/api/register', function(req, res) {
+  const { email, password } = req.body;
+  const user = new User({ email, password });
+  user.save(function(err) {
+    if (err) {
+      res.status(500)
+        .send("Error registering new user please try again.");
+    } else {
+      res.status(200).send("Welcome to the club!");
+    }
+  });
+});
+
+// Connect MongoDB
+mongoose.connect(mongo_uri, { useNewUrlParser: true }, function(err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`Successfully connected to ${mongo_uri}`);
+  }
 });
 
 app.listen(process.env.PORT || 3001);
